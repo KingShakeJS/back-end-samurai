b9a30923913af7778c7575dd903e5438203c3d69
KingShakeJS <kingshakejs@yandex.ru>
2024-11-11 23:30:34 +0300

    l-12

--- dist/index.js
+++ dist/index.js
@@ -5,12 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('express')
 const express_1 = __importDefault(require("express"));
-const tls = require("tls");
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+// const tls = require("tls");
 const app = (0, express_1.default)();
 const port = 3003;
 const jsonBodyMiddleware = express_1.default.json();
 app.use(jsonBodyMiddleware);
-//////////////////////////// 09 37:50
+app.use((0, cors_1.default)());
+app.use((0, body_parser_1.default)());
+//////////////////////////// 12 31:55
 const DataBase = {
     courses: [
         { id: 1, title: 'front1' },
@@ -18,7 +22,20 @@ const DataBase = {
         { id: 3, title: 'front3' },
         { id: 4, title: 'front4' },
         { id: 5, title: 'frontend5' },
-        { id: 5, title: 'frontend6' },
+        { id: 6, title: 'frontend6' },
+    ],
+    products: [
+        { id: 1, title: 'tomato1' },
+        { id: 2, title: 'tomato2' },
+        { id: 3, title: 'tomato3' },
+        { id: 4, title: 'tomato4' },
+        { id: 5, title: 'tomato5' },
+        { id: 5, title: 'ddddd' },
+    ],
+    addresses: [
+        { id: 1, value: 'address1' },
+        { id: 2, value: 'address2' },
+        { id: 3, value: 'address3' },
     ]
 };
 const HTTP_STATUSES = {
@@ -68,6 +85,18 @@ app.post('/courses', (req, res) => {
     DataBase.courses.push(createdCourse);
     res.status(HTTP_STATUSES.CREATED_201).json(createdCourse);
 });
+app.post('/products', (req, res) => {
+    if (!req.body.title) {
+        res.status(HTTP_STATUSES.BAD_REQUEST_400).send('не вышло');
+        return;
+    }
+    const product = {
+        id: +(new Date()),
+        title: req.body.title
+    };
+    DataBase.products.push(product);
+    res.status(HTTP_STATUSES.CREATED_201).json(product);
+});
 app.delete('/courses/:id', (req, res) => {
     DataBase.courses = DataBase.courses.filter(c => c.id !== +req.params.id);
     res.sendStatus(204);
@@ -83,6 +112,59 @@ app.put('/courses/:id', (req, res) => {
         return;
     }
     foundCourse.title = req.body.title;
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
+app.get(`/products`, (req, res) => {
+    let searchString = req.query.title;
+    if (searchString) {
+        let search = searchString.toString();
+        res.send(DataBase.products.filter(p => p.title.indexOf(search) > -1));
+    }
+    res.send(DataBase.products);
+});
+app.get(`/products/:id`, (req, res) => {
+    let product = DataBase.products.find(p => p.id === +req.params.id);
+    if (product) {
+        res.send(product);
+    }
+    else {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUNDED_404);
+        return;
+    }
+});
+app.delete(`/products/:id`, (req, res) => {
+    for (let i = 0; i < DataBase.products.length; i++) {
+        if (DataBase.products[i].id === +req.params.id) {
+            DataBase.products.splice(i, 1);
+            res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+            return;
+        }
+    }
+    res.sendStatus(HTTP_STATUSES.NOT_FOUNDED_404);
+});
+app.get('/addresses', (req, res) => {
+    res.status(HTTP_STATUSES.OK_200).json(DataBase.addresses);
+});
+app.get('/addresses/:id', (req, res) => {
+    const address = DataBase.addresses.find(a => a.id === +req.params.id);
+    if (address) {
+        res.status(HTTP_STATUSES.OK_200).json(address);
+    }
+    else {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUNDED_404);
+    }
+});
+app.put('/products/:id', (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUNDED_404);
+        return;
+    }
+    const foundProduct = DataBase.products.find(c => c.id === +req.params.id);
+    if (!foundProduct) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUNDED_404);
+        return;
+    }
+    foundProduct.title = req.body.title;
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
 });
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 // const express = require('express')
-import express from 'express'
+import express, {Request, Response} from 'express'
+import cors from 'cors'
+import bodyParser from "body-parser";
 
-const tls = require("tls");
+
+// const tls = require("tls");
 const app = express()
 const port = 3003
 const jsonBodyMiddleware = express.json()
 app.use(jsonBodyMiddleware)
-
-//////////////////////////// 09 37:50
+app.use(cors())
+app.use(bodyParser())
+//////////////////////////// 12 31:55
 
 const DataBase = {
     courses: [
@@ -16,7 +20,20 @@ const DataBase = {
         {id: 3, title: 'front3'},
         {id: 4, title: 'front4'},
         {id: 5, title: 'frontend5'},
-        {id: 5, title: 'frontend6'},
+        {id: 6, title: 'frontend6'},
+    ],
+    products: [
+        {id: 1, title: 'tomato1'},
+        {id: 2, title: 'tomato2'},
+        {id: 3, title: 'tomato3'},
+        {id: 4, title: 'tomato4'},
+        {id: 5, title: 'tomato5'},
+        {id: 5, title: 'ddddd'},
+    ],
+    addresses: [
+        {id: 1, value: 'address1'},
+        {id: 2, value: 'address2'},
+        {id: 3, value: 'address3'},
     ]
 }
 const HTTP_STATUSES = {
@@ -27,18 +44,17 @@ const HTTP_STATUSES = {
     BAD_REQUEST_400: 400,
     NOT_FOUNDED_404: 404,
 
-
 }
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.json({message: 'IT-INCUBATOR'})
 })
-app.get('/samurais', (req, res) => {
+app.get('/samurais', (req: Request, res: Response) => {
     res.send('Hello samurais!!!!')
 })
-app.post('/samurais', (req, res) => {
+app.post('/samurais', (req: Request, res: Response) => {
     res.send('мы создали самурая')
 })
-app.get('/courses', (req, res) => {
+app.get('/courses', (req: Request, res: Response) => {
     let foundCourses = DataBase.courses
     if (req.query.title) {
         foundCourses = foundCourses.filter(c => c.title.indexOf(req.query.title as string) > -1)
@@ -51,7 +67,7 @@ app.get('/courses', (req, res) => {
 
 
 })
-app.get('/courses/:id', (req, res) => {
+app.get('/courses/:id', (req: Request, res: Response) => {
     const foundCourse = DataBase.courses.find(c => c.id === +req.params.id);
 
     if (!foundCourse) {
@@ -61,7 +77,7 @@ app.get('/courses/:id', (req, res) => {
     res.json(foundCourse)
 })
 
-app.post('/courses', (req, res) => {
+app.post('/courses', (req: Request, res: Response) => {
 
     if (!req.body.title) {
         res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
@@ -75,13 +91,28 @@ app.post('/courses', (req, res) => {
     DataBase.courses.push(createdCourse)
     res.status(HTTP_STATUSES.CREATED_201).json(createdCourse)
 })
+app.post('/products', (req: Request, res: Response) => {
+
+    if (!req.body.title) {
+        res.status(HTTP_STATUSES.BAD_REQUEST_400).send('не вышло')
+        return
+    }
+
+
+    const product = {
+        id: +(new Date()),
+        title: req.body.title
+    }
+    DataBase.products.push(product)
+    res.status(HTTP_STATUSES.CREATED_201).json(product)
+})
 app.delete('/courses/:id', (req, res) => {
     DataBase.courses = DataBase.courses.filter(c => c.id !== +req.params.id)
 
     res.sendStatus(204)
 })
 
-app.put('/courses/:id', (req, res) => {
+app.put('/courses/:id', (req: Request, res: Response) => {
     if (!req.body.title) {
         res.sendStatus(HTTP_STATUSES.NOT_FOUNDED_404)
         return
@@ -94,6 +125,63 @@ app.put('/courses/:id', (req, res) => {
     }
 
     foundCourse.title = req.body.title
+
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
+})
+
+app.get(`/products`, (req: Request, res: Response) => {
+
+    let searchString = req.query.title
+    if (searchString) {
+        let search = searchString.toString()
+        res.send(DataBase.products.filter(p => p.title.indexOf(search) > -1))
+    }
+    res.send(DataBase.products)
+})
+app.get(`/products/:id`, (req: Request, res: Response) => {
+    let product = DataBase.products.find(p => p.id === +req.params.id)
+    if (product) {
+        res.send(product)
+    } else {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUNDED_404)
+        return
+    }
+
+})
+app.delete(`/products/:id`, (req: Request, res: Response) => {
+    for (let i = 0; i < DataBase.products.length; i++) {
+        if (DataBase.products[i].id === +req.params.id) {
+            DataBase.products.splice(i, 1)
+            res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
+            return
+        }
+    }
+    res.sendStatus(HTTP_STATUSES.NOT_FOUNDED_404)
+})
+app.get('/addresses', (req: Request, res: Response) => {
+    res.status(HTTP_STATUSES.OK_200).json(DataBase.addresses)
+})
+app.get('/addresses/:id', (req: Request, res: Response) => {
+    const address = DataBase.addresses.find(a => a.id === +req.params.id)
+    if (address) {
+        res.status(HTTP_STATUSES.OK_200).json(address)
+    } else {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUNDED_404)
+    }
+})
+app.put('/products/:id', (req: Request, res: Response) => {
+    if (!req.body.title) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUNDED_404)
+        return
+    }
+    const foundProduct = DataBase.products.find(c => c.id === +req.params.id);
+
+    if (!foundProduct) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUNDED_404)
+        return
+    }
+
+    foundProduct.title = req.body.title
 
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })
